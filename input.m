f15fb1f7032e36521c62b7510599df89641afa66
sophie <zvava@disroot.org>
2022-07-30 21:39:07 +0100

    lex pipes

--- input.m
+++ input.m
@@ -1,5 +1,4 @@
-
-!false
-false!
-!true
-true!
+cd("thisPathDoesNotExist")
+!> "notExist" : tty.writeln("invalid path"); return
+!> "toFile"   : tty.writeln("cannot navigate into file"); return
+!> default    | tty.writef("%s λ ", $)
